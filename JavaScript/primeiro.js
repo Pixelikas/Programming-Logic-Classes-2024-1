cc67ee7dce95611459633dcb0cfd974b455ef7f9
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-04-23 20:04:13 -0300

    Add files via upload

---
+++ JavaScript/primeiro.js
@@ -0,0 +1,19 @@
+// Declaração de variáveis
+let idadePessoa = 10
+let nomePessoa = 'Lucas'
+let possuiEbook = false
+let enderecoPessoa
+let numeroUm, numeroDois, somaNumeros
+
+// Entrada de dados com prompt
+numeroUm = Number(prompt('Digite o primeiro número: '))
+numeroDois = Number(prompt('Digite o segundo número: '))
+
+// Cálculos e Lógica
+somaNumeros = numeroUm + numeroDois
+
+// Saída de dados
+alert(somaNumeros)
+
+//alert('Endereço Digitado \n' + enderecoPessoa + '\n\nTchauuuu!')
+
