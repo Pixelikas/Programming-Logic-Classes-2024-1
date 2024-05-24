a54fed72728d15b498d5300b37fdf64b3042a637
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-05-23 23:08:19 -0300

    Add files via upload

---
+++ JavaScript/atividade_77_compra_vinis/Exemplos JS.js
@@ -0,0 +1,20 @@
+
+// Declaração de variáveis
+let nomePessoa
+let idadePessoa = 18
+let possuiPet = true
+let telefonePessoa
+
+let numeroUm, numeroDois
+let somaNumeros
+
+    // Entrada de dados
+    numeroUm= Number(prompt('Digite um número: '))
+    numeroDois= Number(prompt('Digite outro número: '))
+   
+    // Cálculo
+    somaNumeros = numeroUm + numeroDois
+
+    // Saída de dados
+    // alert('Soma dos números: ' + somaNumeros + ' HEHE ' + idadePessoa)
+    alert(`Soma dos números: ${somaNumeros} HEHE ${idadePessoa}`)
