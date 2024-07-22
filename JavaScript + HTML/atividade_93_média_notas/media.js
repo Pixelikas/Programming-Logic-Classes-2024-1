b16b5f5192ff1b2f15406e34805ac56a08a35823
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-07-22 11:40:57 -0300

    Add files via upload

--- JavaScript + HTML/atividade_93_média_notas/media.js
+++ JavaScript + HTML/atividade_93_média_notas/media.js
@@ -1,3 +1,4 @@
+/* Criar um projeto com HTML e JS, onde o usuário possa digitar três notas (inputs) e clicar em um botão para calcular a média (mostrar por alert)*/ 
 
 let elementoInputNotaUm = document.getElementById("inptUm")
 let elementoInputNotaDois = document.getElementById("inptDois")
