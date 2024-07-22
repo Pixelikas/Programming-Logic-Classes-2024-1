cf543180d818abb36bc13f38129998938a5240ad
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-07-22 11:41:19 -0300

    Add files via upload

--- JavaScript + HTML/atividade_96_desconto_produtos/descontoprodutos.js
+++ JavaScript + HTML/atividade_96_desconto_produtos/descontoprodutos.js
@@ -1,3 +1,11 @@
+/* Criar um sistema para compra de produtos + desconto, com HTML e JS.
+O sistema deve deve seguir o layout anexo (imagem).
+
+  - O usuário deverá informar dados de 4 produtos (descrição, quantidade e valor).
+  - Um percentual de desconto deve ser informado.
+  - Ao final deve ser mostrado o valor total da compra com o desconto.
+*/
+
 let qntdUm = document.getElementById("quantidadeUm")
 let vlUm = document.getElementById("valorUm")
 
