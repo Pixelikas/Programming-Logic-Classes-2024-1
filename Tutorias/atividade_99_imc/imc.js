8701d22b1aa2eed89b48ecd674f5e7f3840971cf
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-07-22 11:48:32 -0300

    Add files via upload

--- Tutorias/atividade_99_imc/imc.js
+++ Tutorias/atividade_99_imc/imc.js
@@ -1,3 +1,11 @@
+/* Criar um sistema (de acordo com a imagem) onde o usuário digite seu peso e altura, seja calculado o IMC (peso / altura * altura). Mostrar no campo de IMC o valor do IMC com duas casas após a vírgula, e no campo resultado o resultado de acordo com a tabela abaixo.
+
+IMC menor que 15 -> "Muito abaixo do Peso"
+IMC igual ou maior que 15 e menor que 18 -> "Abaixo do Peso"
+IMC igual ou maior que 18 e menor que 25 -> "Peso Adequado"
+IMC igual ou maior que 25 e menor que 28 -> "Acima do Peso"
+IMC igual ou maior que 28 -> "Muito acima do Peso"
+*/
 
 let elementoInputPeso = document.getElementById('inptPeso')
 let elementoInputAltura = document.getElementById('inptAltura')
