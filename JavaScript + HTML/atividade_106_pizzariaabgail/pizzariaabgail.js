df68eff66923082f6bce7184ef5f5b38f4b770f9
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-07-22 11:44:46 -0300

    Add files via upload

--- JavaScript + HTML/atividade_106_pizzariaabgail/pizzariaabgail.js
+++ JavaScript + HTML/atividade_106_pizzariaabgail/pizzariaabgail.js
@@ -1,3 +1,4 @@
+/* Criar um sistema utilizando HTML e JS, para a Pizzaria da Abgail. Deve conter um título, a mensagem de promoção com 15% de desconto para pedidos acima de R$50 um menu select com 4 sabores, onde um deles custa menos que R$50, outro exatamente R$50, outros dois acima de R$50. Após selecionar o sabor, ao clicar no botão de comprar, devem ser mostrados em dois inputs: o valor do pedido e o valor com desconto se for acima de R$50. */
 
 elementoSelect = document.getElementById('slct')
 elementoInputValor = document.getElementById('inptValor')
