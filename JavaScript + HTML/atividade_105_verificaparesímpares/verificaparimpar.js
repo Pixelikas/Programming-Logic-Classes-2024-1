f93a8d3de0e2cab3e9588e8d044907aa1711976d
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2024-07-22 11:34:29 -0300

    Add files via upload

--- JavaScript + HTML/atividade_105_verificaparesímpares/verificaparimpar.js
+++ JavaScript + HTML/atividade_105_verificaparesímpares/verificaparimpar.js
@@ -16,51 +16,71 @@ let elementoInputResultadoCinco = document.getElementById('inptResultadoCinco')
 
 function VerificaParImpar(){
 
-    if(elementoInputNumeroUm.value % 2 == 0){
+    if(elementoInputNumeroUm.value == ''){
+
+        elementoInputResultadoUm.value = 'Indefinido'
+
+    }else if(elementoInputNumeroUm.value % 2 == 0){
 
         elementoInputResultadoUm.value = 'PAR'
 
-    }else{
+    }else if(elementoInputNumeroUm.value % 2 == 1){
 
         elementoInputResultadoUm.value = 'ÍMPAR'
 
     }
 
-    if(elementoInputNumeroDois.value % 2 == 0){
+    if(elementoInputNumeroDois.value == ''){
+
+        elementoInputResultadoDois.value = 'Indefinido'
+
+    }else if(elementoInputNumeroDois.value % 2 == 0){
 
         elementoInputResultadoDois.value = 'PAR'
 
-    }else{
+    }else if(elementoInputNumeroDois.value % 2 == 1){
 
         elementoInputResultadoDois.value = 'ÍMPAR'
 
     }
 
-    if(elementoInputNumeroTres.value % 2 == 0){
+    if(elementoInputNumeroTres.value == ''){
+
+        elementoInputResultadoTres.value = 'Indefinido'
+
+    }else if(elementoInputNumeroTres.value % 2 == 0){
 
         elementoInputResultadoTres.value = 'PAR'
 
-    }else{
+    }else if(elementoInputNumeroTres.value % 2 == 1){
 
         elementoInputResultadoTres.value = 'ÍMPAR'
 
     }
 
-    if(elementoInputNumeroQuatro.value % 2 == 0){
+    if(elementoInputNumeroQuatro.value == ''){
+
+        elementoInputResultadoQuatro.value = 'Indefinido'
+
+    }else if(elementoInputNumeroQuatro.value % 2 == 0){
 
         elementoInputResultadoQuatro.value = 'PAR'
 
-    }else{
+    }else if(elementoInputNumeroQuatro.value % 2 == 1){
 
         elementoInputResultadoQuatro.value = 'ÍMPAR'
 
     }
 
-    if(elementoInputNumeroCinco.value % 2 == 0){
+    if(elementoInputNumeroCinco.value == ''){
+
+        elementoInputResultadoCinco.value = 'Indefinido'
+
+    }else if(elementoInputNumeroCinco.value % 2 == 0){
 
         elementoInputResultadoCinco.value = 'PAR'
 
-    }else{
+    }else if(elementoInputNumeroCinco.value % 2 == 1){
 
         elementoInputResultadoCinco.value = 'ÍMPAR'
 
